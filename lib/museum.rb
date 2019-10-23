7b08d191108573fefbe4e9f230c922f5daad535a
Alison Vermeil <alison.minton@gmail.com>
2019-10-23 10:24:13 -0600

    Add patrons_by_exhibit_interest method and tests

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,8 +1,34 @@
 class Museum
-  attr_reader :name, :exhibits
-  
+  attr_reader :name, :exhibits, :patrons
+
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    recommendations = []
+    patron.interests.each do |interest|
+      recommendations << @exhibits.find { |exhibit| exhibit.name == interest }
+    end
+    recommendations
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    @exhibits.reduce({}) do |hash, exhibit|
+
+      hash[exhibit] = @patrons.find_all { |patron| patron.interests.include?(exhibit.name)}
+      hash
+    end
+  end
+
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,11 +1,19 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/patron'
 require './lib/exhibit'
 require './lib/museum'
 
 class MuseumTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+
+    @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    @imax = Exhibit.new("IMAX", 15)
+
+    @bob = Patron.new("Bob", 20)
+    @sally = Patron.new("Sally", 20)
   end
 
   def test_it_exists
@@ -15,6 +23,61 @@ class MuseumTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+    assert_equal [], @dmns.patrons
   end
 
+  def test_it_can_add_exhibit
+    assert_equal [], @dmns.exhibits
+
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_it_can_recommend_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+
+
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+
+    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@bob)
+
+    @sally.add_interest("IMAX")
+
+    assert_equal [@imax], @dmns.recommend_exhibits(@sally)
+  end
+
+  def test_it_can_admit_patrons
+    assert_equal [], @dmns.patrons
+
+    @dmns.admit(@bob)
+    @dmns.admit(@sally)
+
+    assert_equal [@bob, @sally], @dmns.patrons
+  end
+
+  def test_it_can_sort_patrons_by_exhibit_interest
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+
+    @sally.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(@bob)
+    @dmns.admit(@sally)
+
+    assert_equal (
+      {@gems_and_minerals => [@bob], @dead_sea_scrolls => [@bob, @sally],
+      @imax => []
+      }), @dmns.patrons_by_exhibit_interest
+  end
 end
